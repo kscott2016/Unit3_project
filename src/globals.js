49ccfcce8b9c0e13e007c7a2921e3056f3993e9a
Krystina <krystinascott2008@yahoo.com>
2022-04-27 16:33:55 -0500

    clean up global file

--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,3 @@
 export const BASE_URL = 'https://api.lazydesignerexcuses.com/random'
+
 export const ALL_EXCUSES_URL = 'https://api.lazydesignerexcuses.com/all'
-
-//export const POSTER_PATH = 'https://image.tmdb.org/t/p/original'
-
-//export const  API_KEY = process.env.REACT_APP_TMDB_KEY
